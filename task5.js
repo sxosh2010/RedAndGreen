02c4bb85bd1533020a595aed67569bc7d4aec990
xolelwa <xolelwa@projectcodex.co>
2015-11-05 12:04:08 +0200

    task5

--- task5.js
+++ task5.js
@@ -1,3 +1,26 @@
-var setTestStatus = function(testresult, testbutton, passing){
-setTestStatus.addEventlister
-}
+// Create an implementation for the setTestStatus function.
+var setTestStatus = function (elementId, buttonId, className){
+// Remember that it takes three parameters: the id of the element; the id of the button; the test status.
+    var elem = document.getElementById(elementId); 
+
+    elem.className = className;
+
+   var button = document.getElementById(buttonId);
+
+   if (className === "failing") {
+    button.innerHTML = "Make passing";
+  }
+
+
+  else {
+    button.innerHTML ="Make failing";
+ }
+
+};
+
+
+// Refresh the page and see what happens.
+// There should be no errors in Chrome DevTools.
+// You should see a console message displaying 'success'.
+	
+
--- task5_tests.js
+++ task5_tests.js
@@ -1,55 +1,85 @@
 //    Check that the div with id testresult doesn't have the class failing or passing. 
 //    Use console.log to display a message that tells you what the result of the check was.
+console.log("*******start*******")
 var testresult = document.getElementById('testresult');
 
-if (testresult.className === "grey"){
-	console.log("the class name is passing : " + testresult.className);	
+var className = testresult.className;
+
+if (className === "grey"){
+	console.log("the class name is correct : " + testresult.className);	
 }
 else{
-	console.log("the class name is failing : " + testresult.className)
+	console.log("the class name is incorrect its supposed to be grey  but its : " + testresult.className)
 }
 
 //    Check that the button has the text 'Make failing'.
-var mybutton = document.getElementById("button");
-    if(mybutton  === 'make failing'){
-    	console.log("the button text is  : " + mybutton.innerHTML);
+ var button = document.getElementById("testbutton");
+
+    if (button.innerHTML === "Make failing") {
+    	console.log("the button text Make failing is  correct : " + button.innerHTML);
     } 
-    else{
-      console.log("the button text is   : " + mybutton.innerHTML);
+    else {
+      console.log("the button text is  incorrect : " + button.innerHTML);
 
     }
 
 
 //    Call setTestStatus('testresult', 'testbutton' , 'failing');
+console.log("*******setTestStatus is called for failing******")
+         setTestStatus('testresult', 'testbutton' , 'failing');
 
 
-//     Check that #testresult has the class failing. The CSS should give it a red background.
-if (testresult.className === "failing"){
-	console.log("the testresult class is  : " +testresult.className);
-}
+ //     Check that #testresult has the class failing. The CSS should give it a red background.
+ 
+ if (testresult.className === "failing"){
+ 	console.log("the testresult class is  correct  the way it is  : " + testresult.className);
+ }
 else{
-	console.log("the testresult class is  : ");
-}
+	console.log("the testresult class is  incorrect: " + testresult.className);
+ }
 
-//     Check that the button has the text 'Make passing'.
-var mybutton = document.getElementById("button");
+ //     Check that the button has the text 'Make passing'.
+var button = document.getElementById("testbutton");
 
-if (mybutton!== "make passing"){
-console.log("the button text is   :");
-}
-else{
-console.log("the button text is   : ");
-} 
+var innerHTML = testbutton.innerHTML;
+
+if (innerHTML === "Make passing"){
+console.log("the button text is  passing  so its  correct  :" + button.innerHTML);
+ }
+else {
+ console.log("the testbutton text is not passing  but it  is: " + button.innerHTML);
+ } 
 //     Call setTestStatus('testresult', 'passing');
-         
+console.log("*******setTestStatus is called for passing******")
 
-//     Check that #testresult has the class passing. The CSS should give it a green background.
+        setTestStatus('testresult', 'testbutton'  ,'passing');
+          
+
+ //     Check that #testresult has the class passing. The CSS should give it a green background.
 if (testresult.className = "passing") {
-	console.log("");
-};
+	console.log("the testresult class is green is correct thats what i want:" +  testresult.className);
+}
+ else{
+    console.log("the  testresult class is  failing: " + testresult.className);
+}
 
-//     Check that the button has the text 'Make red'.
+ //     Check that the button has the text 'Make red'.
+         var button = document.getElementById("testbutton");
+        var innerHTML = button.innerHTML;
 
 
-//     Display 'success' if testresult has the class passing, otherwise display 'failure'.
+         if(innerHTML === "Make red"){
+            console.log("the button text  is   Make failing its correct : " + button.innerHTML);
 
+         }
+         else{
+            console.log( "the button text is  incorrect its supposed to  be passing " + button.innerHTML);
+          }
+ //     Display 'success' if testresult has the class passing, otherwise display 'failure'.
+        if(testresult.className === "passing"){
+             console.log("success");
+
+         }
+        else {
+            console.log("failure");
+        }
